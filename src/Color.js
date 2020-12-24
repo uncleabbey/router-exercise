beccb5c4d433f1f62646a73a7f94f39ffcd22452
Kayode Gabriel Abiodun <kayodegabriela@gmail.com>
2020-12-24 11:57:02 +0100

    add some correction

--- src/Color.js
+++ src/Color.js
@@ -1,34 +1,39 @@
-import React, { Component } from 'react'
-import { Link, Redirect } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link, Redirect } from "react-router-dom";
 
 export class Color extends Component {
   state = {
-    color: {}
-  }
+    color: {},
+  };
   componentDidMount() {
     const colors = JSON.parse(localStorage.getItem("colors")) || [];
     const { color } = this.props.match.params;
-    const findColor = colors.find((item) => item.colorName === color)
-          this.setState({
-        color: findColor
-      })
+    const findColor = colors.find((item) => item.colorName === color);
+    this.setState({
+      color: findColor,
+    });
   }
-  
+
   render() {
-    if(!this.state.color) {
-      return <Redirect to="/" />
+    if (!this.state.color) {
+      return <Redirect to="/" />;
     }
     // console.log(this.state.color.colorValue)
     return (
-      <div className="color-details" style={{background: this.state.color.colorValue}}>
-       
+      <div
+        className="color-details"
+        style={{ background: this.state.color.colorValue }}
+      >
         <h1>This is color {this.state.color.colorName} </h1>
 
         <h1> Isn't it Beautiful </h1>
-        <h1> <Link to="/">Go back</Link></h1>
+        <h1>
+          {" "}
+          <Link to="/">Go back</Link>
+        </h1>
       </div>
-    )
+    );
   }
 }
 
-export default Color
+export default Color;
--- src/Colors.js
+++ src/Colors.js
@@ -1,24 +1,32 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 // import  from 'react-router-dom'
 
-const Color = ({ color }) => <Link to={`/colors/${color}`}><li>{color}</li></Link>
+const Color = ({ color }) => (
+  <Link to={`/colors/${color}`}>
+    <li>{color}</li>
+  </Link>
+);
 
 export class Colors extends Component {
   state = {
-    colors: JSON.parse(localStorage.getItem("colors")) || []
-  }
+    colors: JSON.parse(localStorage.getItem("colors")) || [],
+  };
   render() {
     const { colors } = this.state;
-    const data = colors.map(({ colorValue, colorName }, index) => <Color key={index} color={colorName} />)
+    const data = colors.map(({ colorValue, colorName }, index) => (
+      <Color key={index} color={colorName} />
+    ));
     return (
       <div className="color-container">
         <h1 className="title">List of Colors</h1>
-        <Link to="/colors/new"><button>Add Color </button></Link>
+        <Link to="/colors/new">
+          <button>Add Color </button>
+        </Link>
         <ul>{data}</ul>
       </div>
-    )
+    );
   }
 }
 
-export default Colors
+export default Colors;
